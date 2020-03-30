a7578db717511df69c8697b80accf8f53a203b63
fery_rz <feryan.romadhon@gmail.com>
2020-03-30 18:55:28 +0700

    Update COVID19_Monitoring_ESP32.ino
    Remove Comment

--- COVID19_Monitoring_ESP32/COVID19_Monitoring_ESP32.ino
+++ COVID19_Monitoring_ESP32/COVID19_Monitoring_ESP32.ino
@@ -82,29 +82,3 @@ void loop() {
 
   delay(10000);   // delay 10 detik
 }
-
-//  http.begin("http://siagacorona.semarangkota.go.id/halaman/cirini_bingsit/");
-//  int httpCode = http.GET();
-//
-//  if (httpCode > 0) {
-//    // HTTP header has been send and Server response header has been handled
-//    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-//
-//    // file found at server
-//    if (httpCode == HTTP_CODE_OK) {
-//      String payload = http.getString();
-//
-//      int start = payload.indexOf("[{", payload.indexOf("[{") + 1);
-//      int end = payload.indexOf("}]") + 2;
-//
-//      Serial.println(start);
-//      Serial.println(end);
-//
-//      String data = payload.substring(start, end);
-//      data.trim();
-//      Serial.println(data);
-//
-//    }
-//  } else {
-//    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-//  }
